9b4699de7d4b8fb29442047e7de53bf0c5a34ae6
novalrnv <akuunoval@gmail.com>
2026-02-19 15:33:09 +0700

    Controller

---
+++ JS2/praktikum/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama : Khoirul Umam Novalidi <br>NIM : 244107020210';
+    }
+}
--- JS2/praktikum/routes/web.php
+++ JS2/praktikum/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya ' .$name;
@@ -10,23 +17,15 @@ Route::get('/articles/{id}', function ($id) {
     return 'Halaman Artikel dengan ID '.$id; 
 });
 
-Route::get('/posts/{post}/comments/{comment}', function 
-($postId, $commentId) { 
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
-});
+Route::get('/posts/{postid}/comments/{commentid}', 
+    [ArticleController::class, 'articles']);
 
-Route::get('/about', function () {
-    return 'Nama : Khoirul Umam Novalidi <br>NIM : 244107020210';
-});
+Route::get('/about', [AboutController::class, 'about']);
 
-Route::get('/letter', function () {
-    return 'Selamat Datang';
-});
+Route::get('/letter', [HomeController::class, 'index']);
 
 Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::get('/hello', [WelcomeController::class, 'hello']);
