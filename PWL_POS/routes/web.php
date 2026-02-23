8e36e54bdcbad138f8649458349048bddfe7ab41
novalrnv <akuunoval@gmail.com>
2026-02-24 00:33:52 +0700

    DB Facade

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
+
+Route::get('/level', [LevelController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
