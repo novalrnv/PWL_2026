937e694f75197f762ebee7b4a24456c060cdfb9c
novalrnv <akuunoval@gmail.com>
2026-02-23 17:10:21 +0700

    User

---
+++ JS2/POS/resources/views/user/profile.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>User Profile - POS</title>
+</head>
+<body>
+    <h1>User Profile</h1>
+    <hr>
+    <p><strong>User ID :</strong> {{ $id }}</p>
+    <p><strong>User Name :</strong> {{ $name }}</p>
+</body>
+</html>
--- JS2/POS/routes/web.php
+++ JS2/POS/routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
 
 Route::get('/home', [HomeController::class, 'index']);
 
