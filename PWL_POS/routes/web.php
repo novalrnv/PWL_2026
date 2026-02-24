e82f61152e6d6695af4061c79b60cdecd438109f
novalrnv <akuunoval@gmail.com>
2026-02-24 08:58:09 +0700

    Eloquent Orm

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
+Route::get('/user', [UserController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
 
