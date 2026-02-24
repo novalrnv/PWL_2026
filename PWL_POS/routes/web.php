d0bfb55d5f580eb81c529f04b343f875b0a828b6
novalrnv <akuunoval@gmail.com>
2026-02-24 08:06:25 +0700

    Query Builder

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
+Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
 
 Route::get('/', function () {
